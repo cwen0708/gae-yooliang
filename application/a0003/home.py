464f649d4767f38281b2224bfb01d6655d0997d7
QiLiang Wen <cwen0708@gmail.com>
2013-10-31 13:13:40 +0800

    更新

--- application/a0003/home.py
+++ application/a0003/home.py
@@ -13,34 +13,28 @@ class Index(HomeHandler):
         size = self.params.get_integer("size", 10)
         page = self.params.get_integer("page", 1)
         self.list = self.sql.query_all('SELECT * FROM PastCase WHERE is_enable = 1 AND is_delete = 0 ORDER BY sort DESC LIMIT %s, %s', ((page - 1) * size, size))
-        for item in self.list:
-            item["first_image"] = u""
-            if item["images"] is not None and item["images"] is not u"" and item["images"] is not "":
-                images = item["images"].split(",")
-                item["first_image"] = images[0]
-                item["image_list"] = images
         random.shuffle(self.list)
         try:
             self.link_01 = "case_view.html?id=" + str(self.list[0]["id"])
-            self.image_01 = self.list[0]["first_image"]
+            self.image_01 = self.list[0]["image"]
         except IndexError:
             self.link_01 = "case_list.html"
             self.image_01 = "images/testimg01.jpg"
         try:
             self.link_02 = "case_view.html?id=" + str(self.list[1]["id"])
-            self.image_02 = self.list[1]["first_image"]
+            self.image_02 = self.list[1]["image"]
         except IndexError:
             self.link_02 = "case_list.html"
             self.image_02 = "images/testimg02.jpg"
         try:
             self.link_03 = "case_view.html?id=" + str(self.list[2]["id"])
-            self.image_03 = self.list[2]["first_image"]
+            self.image_03 = self.list[2]["image"]
         except IndexError:
             self.link_03 = "case_list.html"
             self.image_03 = "images/testimg01.jpg"
         try:
             self.link_04 = "case_view.html?id=" + str(self.list[3]["id"])
-            self.image_04 = self.list[3]["first_image"]
+            self.image_04 = self.list[3]["image"]
         except IndexError:
             self.link_04 = "case_list.html"
             self.image_04 = "images/testimg02.jpg"
@@ -67,9 +61,8 @@ class CaseList(HomeHandler):
         self.list = self.sql.query_all('SELECT * FROM PastCase WHERE is_enable = 1 AND is_delete = 0 ORDER BY sort DESC LIMIT %s, %s', ((page - 1) * size, size))
         for item in self.list:
             if item["images"] is not None and item["images"] is not u"" and item["images"] is not "":
-                images = item["images"].split(",")
-                item["first_image"] = images[0]
-                item["image_list"] = images
+                item["image_list"] = item["images"].split(",")
+
         self.prev_page = page - 1 if page > 1 else 1
         self.next_page = page + 1 if page < self.page_all else self.page_all
         if self.is_ajax:
@@ -79,9 +72,8 @@ class CaseView(HomeHandler):
     def get(self, *args):
         self.record = self.sql.query_by_id("PastCase", self.params.get_integer("id"))
         if self.record["images"] is not None and self.record["images"] is not u"" and self.record["images"] is not "":
-            images = self.record["images"].split(",")
-            self.record["first_image"] = images[0]
-            self.record["image_list"] = images
+            self.record["image_list"] = self.record["images"].split(",")
+
         if self.is_ajax:
             self.render("/case_view_ajax.html")
 
@@ -95,9 +87,8 @@ class HotList(HomeHandler):
         self.list = self.sql.query_all('SELECT * FROM HotCase WHERE is_enable = 1 AND is_delete = 0 ORDER BY sort DESC LIMIT %s, %s', ((page - 1) * size, size))
         for item in self.list:
             if item["images"] is not None and item["images"] is not u"" and item["images"] is not "":
-                images = item["images"].split(",")
-                item["first_image"] = images[0]
-                item["image_list"] = images
+                item["image_list"] = item["images"].split(",")
+
         self.prev_page = page - 1 if page > 1 else 1
         self.next_page = page + 1 if page < self.page_all else self.page_all
         if self.is_ajax:
@@ -107,8 +98,7 @@ class HotView(HomeHandler):
     def get(self, *args):
         self.record = self.sql.query_by_id("HotCase", self.params.get_integer("id"))
         if self.record["images"] is not None and self.record["images"] is not u"" and self.record["images"] is not "":
-            images = self.record["images"].split(",")
-            self.record["first_image"] = images[0]
-            self.record["image_list"] = images
+            self.record["image_list"] = self.record["images"].split(",")
+
         if self.is_ajax:
             self.render("/hot_view_ajax.html")
