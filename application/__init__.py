f08706f6133950ef709567bda2c498551184bee9
QiLiang Wen <cwen0708@gmail.com>
2013-11-02 06:28:08 +0800

    更新

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,7 @@
 
 RELATIONSHIP = [
     {"app": u"a0002", "name": u"開發環境", "host": u"localhost"},
+    {"app": u"a0000", "name": u"測試環境", "host": u"5.yooliang-technology.appspot.com"},
     {"app": u"a0000", "name": u"侑良科技", "host": u"yooliang-technology.appspot.com"},
     {"app": u"a0000", "name": u"侑良科技", "host": u"www.yooliang.com"},
     {"app": u"a0001", "name": u"一品夫人", "host": u"www.063318866.com"},
